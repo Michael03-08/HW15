845db17851f7c8e9afebfd0a7bca2de8e84801a3
Flex <cma491628@gmail.com>
2023-12-09 20:07:20 +0200

    Работа со строками

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -3,7 +3,17 @@ public class Main {
     // А-123
     // Б-321...
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
+    static String letter = "абвгдеёжзийклмнеопрстуфхцчшщъьэюя";
+    static String capitalLetter = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЭЮЯ";
     public static void main(String[] args) {
         //todo код писать тут
+
+        for (int i = 0; i < letter.length(); i++){
+            System.out.println(letter.charAt(i) +" - " + letter.codePointAt(i));
+        }
+        for (int i = 0; i < capitalLetter.length(); i++){
+            System.out.println(capitalLetter.charAt(i) + " - " + capitalLetter.codePointAt(i));
+        }
     }
+
 }
--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+    public static final String CONTROL_LETTERS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЭЮЯабвгдеёжзийклмнеопрстуфхцчшщъьэюя";
+
     public static void main(String[] args) {
         //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите фамилию, имя, отчество: ");
+        String fullName = scanner.nextLine().trim();
+        control(fullName);
+    }
+    public static void control(String input){
+
+        boolean isValid = true;
+        int controlInput = 0;
+
+        for (int i = 0; i < input.length(); i++){
+            if (input.charAt(i) == ' '){
+                controlInput++;
+            }
+            if (!CONTROL_LETTERS.contains(String.valueOf(input.charAt(i)).trim())){
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid && controlInput == 2){
+            System.out.println("Фамилия " + input.substring(0, input.indexOf(" ")).trim());
+            System.out.println("Имя " + input.substring(input.indexOf(" "),
+                    input.lastIndexOf(" ")).trim());
+            System.out.println("Отчество " + input.substring(input.lastIndexOf(" ")).trim());
+
+        }else {
+            System.out.println("Введенная строка не является Ф.И.О");
+        }
     }
 }
